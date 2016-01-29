fa2897fb4c006555add9386c1b5e9e24ed84af69
Edvardius <mtyszkiewicz@gmail.com>
2016-01-30 00:50:52 +0100

    pies i jego kontroler

---
+++ Assets/Scripts/DemonDogBehavior.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DemonDogBehavior : MonoBehaviour
+{
+    public GameObject Player;
+    public GameObject Portal;
+
+	void Start ()
+    {
+        this.gameObject.transform.position = Portal.gameObject.transform.position;
+    }
+	
+	void Update ()
+    {
+        // jakieś chodzenie tego psa, żeby losowo łaził, ale strzelał w Playera co 3 sekundy + losowy timer między 0-3 sekund
+        //this.gameObject.transform.position = Player.gameObject.transform.position.Normalize();
+	}
+}
