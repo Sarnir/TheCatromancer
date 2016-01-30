282bad0f0bb80867445fa3dc96c8bdd3b3fa9535
PiWo <piotr.wolski@playsofr.fr>
2016-01-30 11:11:59 +0100

    - added projectile

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -1,19 +1,39 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[RequireComponent(typeof(SpriteRenderer))]
 public class Projectile : MonoBehaviour
 {
     [SerializeField]
     float speed = 1f;
 
+    SpriteRenderer renderer;
+    bool seen;
+
     public Vector2 direction { get; set; }
 
-    void FixedUpdate()
+    void Start()
     {
+        renderer = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        if (renderer.isVisible)
+            seen = true;
+
+        if (seen && !renderer.isVisible)
+            Destroy();
+
         transform.position += new Vector3(direction.x * speed, direction.y * speed, 0);
     }
 
     void OnTriggerEnter2D(Collider2D other)
+    {
+        Destroy();
+    }
+
+    void Destroy()
     {
         Destroy(gameObject);
     }
