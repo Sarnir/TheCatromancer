52e4f2f194feaa3f48bd8d39f39429312567e0c5
Edvardius <mtyszkiewicz@gmail.com>
2016-01-30 23:04:29 +0100

    EDużo

---
+++ Assets/Scripts/GameOverController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverController : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) || Input.GetKeyUp(KeyCode.Return) ||
+            Input.GetKeyUp(KeyCode.Escape) || Input.GetKeyUp(KeyCode.Space))
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+}
