3201fe7de231d7ce32f7517e4c357ae27685812d
Edvardius <mtyszkiewicz@gmail.com>
2016-01-30 01:17:54 +0100

    asds

--- Assets/Scripts/DemonDogBehavior.cs
+++ Assets/Scripts/DemonDogBehavior.cs
@@ -1,18 +1,23 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
 public class DemonDogBehavior : MonoBehaviour
 {
     public GameObject Player;
     public GameObject Portal;
 
+    private DateTime ShotInterval;
+
 	void Start ()
     {
         this.gameObject.transform.position = Portal.gameObject.transform.position;
+        ShotInterval = DateTime.Now;
     }
 	
 	void Update ()
     {
+
         // jakieś chodzenie tego psa, żeby losowo łaził, ale strzelał w Playera co 3 sekundy + losowy timer między 0-3 sekund
         //this.gameObject.transform.position = Player.gameObject.transform.position.Normalize();
 	}
