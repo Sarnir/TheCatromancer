503cbd45cb09b7de800269edfa45ed5e9a6ef4c4
PiWo <piotr.wolski@playsofr.fr>
2016-01-30 01:21:08 +0100

    - Added projectile

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Projectile : MonoBehaviour
+{
+    [SerializeField]
+    float speed = 1f;
+
+    public Vector2 direction { get; set; }
+
+    void FixedUpdate()
+    {
+        transform.position += new Vector3(direction.x * speed, direction.y * speed, 0);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Destroy(gameObject);
+    }
+}
